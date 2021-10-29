124ab60ad84a7b8c757e9e1ea6e5d45cb1de0f6e
Biswajit <biswajit.majumdar@carnationit.com>
2021-10-29 21:56:59 +0530

    First2

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div class="row">
           <div class="col-sm-2">
             <h3>Column 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Lorem ipsum dolor sit amet,  consectetur adipisicing elit...</p>
             <p>
               Ut enim ad minim veniam, quis nostrud exercitation ullamco
               laboris...
